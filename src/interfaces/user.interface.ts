f906d82edc39d7a8eaa8abfd71b074a7f816f2cf
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-10-28 21:27:01 -0600

    feat: extend user interface with creation attributes and optional password

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,7 +1,13 @@
+import type { Optional } from "sequelize";
+
 export interface UserAtributes {
   id: number;
   name: string;
   email: string;
   password?: string;
   id_role: number;
+}
+
+export interface UserCreationAttributes extends Optional<UserAtributes, "id"> {
+  password: string;
 }
